f9dda62af546d06c397b1d616b490344cc40bcce
myriam <myriam.schoenenberger@gmail.com>
2024-11-15 11:20:09 +0900

    udpated, rrt* to be fixed

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import math
 
 # Define environment and obstacles
 class Environment:
@@ -17,13 +18,23 @@ class Environment:
             if obstacle.contains_point((node.x, node.y)):
                 return False
         return True
+    
 
-# Node Classe
 class Node:
     def __init__(self, x, y, parent=None):
         self.x = x
         self.y = y
-        self.parent = parent # for assigning 
+        self.parent = parent  # parent ref
+        self.cost = 0.0 if parent is None else parent.cost + node_distance(self, parent)  # Initialize cost
+
+    def __repr__(self):
+        return f"Node(x={self.x}, y={self.y}, cost={self.cost:.2f})"
+    
+    def distance_to(self, other):
+        """Calculate Euclidean distance to another node."""
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+    
+
 
 # Sample a random point in the environment and return as node without parent
 def sample_random_point(env):
@@ -34,6 +45,8 @@ def sample_random_point(env):
 def sample_random_node(nodes):
     return random.choice(nodes)
 
+
+
 def get_nearest_node(nodes, random_node):
     nearest_node = nodes[0]
     min_distance = np.linalg.norm([nearest_node.x - random_node.x, nearest_node.y - random_node.y])
@@ -43,6 +56,69 @@ def get_nearest_node(nodes, random_node):
         if distance < min_distance:
             nearest_node = node
             min_distance = distance
-            
     return nearest_node
 
+
+
+def extend_tree_towards(xtree, xrand, max_step_size):
+    # Compute the direction vector from xtree to xrand
+    direction = np.array([xrand.x - xtree.x, xrand.y - xtree.y])
+    distance = np.linalg.norm(direction)  # Euclidean distance between xtree and xrand
+    
+    if distance > max_step_size:
+        # Scale the direction to the max step size
+        direction = direction / distance * max_step_size
+        new_x = xtree.x + direction[0]
+        new_y = xtree.y + direction[1]
+    else:
+        new_x = xrand.x
+        new_y = xrand.y
+    
+    return Node(new_x, new_y, parent=xtree  )  # Return the new node
+
+
+def check_goal_reached(node, goal_node, goal_radius):
+    # Calculate the distance between the current node and the goal
+    distance_to_goal = np.linalg.norm([node.x - goal_node.x, node.y - goal_node.y])
+    return distance_to_goal <= goal_radius
+
+
+# def find_neighbors(nodes, new_node, radius):
+    # """Return a list of nodes within a specified radius from new_node."""
+    # return [node for node in nodes if node_distance(node, new_node) <= radius]
+# 
+
+def find_neighbors(nodes, x, gamma=0.75, d=2, max_threshold=0.50, min_threshold=0.0001):
+    n = len(nodes)  # Total number of nodes in the tree
+    threshold = min(gamma * (math.log(n) / n) ** (1 / d)+0.0001, 10.)  #  threshold distance
+
+    print(threshold)
+    
+    neighbors = []
+
+    for node in nodes:
+        # Calculate Euclidean distance between node and point x
+        distance = np.linalg.norm(np.array([node.x, node.y]) - np.array([x.x, x.y])) 
+        if distance <= threshold:
+            neighbors.append(node)
+
+    while len(neighbors) == 0:
+        threshold *= 1.2
+        print(f"No neighbors found. New threshold: {threshold}")
+        
+        # Recalculate neighbors with the updated threshold
+        neighbors = []
+        for node in nodes:
+            distance = np.linalg.norm(np.array([node.x, node.y]) - np.array([x.x, x.y])) 
+            if distance <= threshold:
+                neighbors.append(node)
+
+        # Optionally, stop if the threshold exceeds a maximum value
+        if threshold > max_threshold :  # You can adjust this multiplier as needed
+            print("Threshold exceeded maximum limit, stopping search.")
+            break
+    
+    return neighbors
+
+def node_distance(node, goal_node):
+    return ((node.x - goal_node.x) ** 2 + (node.y - goal_node.y) ** 2) ** 0.5
--- main.py
+++ main.py
@@ -8,43 +8,31 @@ from PIL import Image
 import gc  # For manual garbage collection
 import random
 from env import *
+from utils import *
+import gradio as gr
+import time
+
 
 # Hyperparameters
-GOAL_RADIUS = 0.3  # Threshold distance to goal for termination
 STEP_SIZE = 0.5  # You can adjust this value as needed
+GOAL_RADIUS = STEP_SIZE
 MAX_STEPS = 50 # let's not go to inifinity and beyond!
 GIF_FOLDER = 'randTree_1p1_frames'  # Folder to save frames
-SAVE_INTERVAL = 100
+FINAL_FRAME_PATH = os.path.join(GIF_FOLDER, "final_frame.png")
+REWIRE_RADIUS = 0.7
 
-def extend_towards_random(xtree, xrand, max_step_size=STEP_SIZE):
-    # Compute the direction vector from xtree to xrand
-    direction = np.array([xrand.x - xtree.x, xrand.y - xtree.y])
-    distance = np.linalg.norm(direction)  # Euclidean distance between xtree and xrand
-    
-    if distance > max_step_size:
-        # Scale the direction to the max step size
-        direction = direction / distance * max_step_size
-        new_x = xtree.x + direction[0]
-        new_y = xtree.y + direction[1]
-    else:
-        new_x = xrand.x
-        new_y = xrand.y
-    
-    return Node(new_x, new_y, parent=xtree  )  # Return the new node
-
-
-def check_goal_reached(node, goal_node, goal_radius=GOAL_RADIUS):
-    # Calculate the distance between the current node and the goal
-    distance_to_goal = np.linalg.norm([node.x - goal_node.x, node.y - goal_node.y])
-    return distance_to_goal <= goal_radius
-
-
+# Global variables to track simulation state
+global_nodes = None
+global_step_count = 0
+global_goal_reached = False
+global_shortest_cost = float('inf')
 
 # Set up environment dimensions and obstacles
 env = Environment(10, 10)
-env.add_obstacle(Circle((3, 2.2), 1.8 ))  # circle
+env.add_obstacle(Circle((3, 2.2), 1.3 ))  # circle
 env.add_obstacle(Rectangle((2, 6), 1, 3 ))  # rectancle
-env.add_obstacle(Rectangle((5.5, 0.5), 3.5, 3.5))  #  square
+env.add_obstacle(Rectangle((5.5, 0.5), 3, 3))  #  square
+
 env.add_obstacle(Rectangle((7.05, 8.75), 0.5, 2.5, angle=140))  # m line left
 env.add_obstacle(Rectangle((6.8, 7.2), 0.5, 1.5, angle=10))  # m middle left
 env.add_obstacle(Rectangle((8.5, 7.1), 0.5, 1.7, angle=90))  # m middle right
@@ -63,130 +51,168 @@ output_folder = "motion_planning_steps"
 os.makedirs(output_folder, exist_ok=True)
 
 
-# Draw environment and obstacles
-def draw_environment(env, nodes, goal_node, step, folder_path, goal_reached = False):
-    fig, ax = plt.subplots(figsize=(6, 6))
-    # Draw obstacles here if needed (using ax.add_patch())
+
+
+
+
+
+# Function to run a specific number of steps in the simulation
+def run_simulation_streaming(steps, algorithm_choice = "RT", heuristic_probability=0): 
+    global global_nodes, global_step_count, global_goal_reached, global_shortest_cost
     
-    # Draw nodes and edges
-    for node in nodes:
-        ax.plot(node.x, node.y, 'go')  # 'go' is green dot for nodes
-        if node.parent:
-            ax.plot([node.x, node.parent.x], [node.y, node.parent.y], 'g-', linewidth=1)
+    if global_nodes is None:  # Reset state if no existing nodes
+        start_node = Node(5, 5)
+        global_nodes = [start_node]
+        global_step_count = 0
+        global_goal_reached = False
+        global_shortest_cost = float('inf')
+        # Clear GIF folder
+        if os.path.exists(GIF_FOLDER):
+            for file in os.listdir(GIF_FOLDER):
+                os.remove(os.path.join(GIF_FOLDER, file))
     
-    # Draw the start and goal nodes
-    plt.plot(nodes[0].x, nodes[0].y, 'bo', markersize=6, label="Start Node")
-    plt.plot(goal_node.x, goal_node.y, 'ro', markersize=6, label="Goal Node")
-    
+    goal_node = Node(9, 9)
 
-    # Highlight the latest node in orange
-    new_node = nodes[-1]
-    plt.plot(new_node.x, new_node.y, 'o', color='orange', markersize=5)
+    start_time = time.time()  # Start timer
 
-    # Draw shortest path in red
-    draw_shortest_path(ax, nodes, goal_node, goal_reached)
-    
-    # Add a legend
-    plt.legend(loc="upper right")
-    
+    for _ in range(steps):
+        # sample until you find a point without collissions.
+        while True:
+            random_point = sample_random_point(env)
+            parent_node = None
 
-    # Draw obstacles
-    for obstacle in env.obstacles:
-        ob_cop = copy(obstacle)
-        ax.add_patch(ob_cop)
+            if algorithm_choice == "RT":
+                # Heuristic-based node selection
+                if random.random() < heuristic_probability:  # Select nearest node based on heuristic
+                    parent_node = get_nearest_node(global_nodes, goal_node)
+                else:  # Otherwise, select a random node
+                    parent_node = sample_random_node(global_nodes)
+                
+                # Try extending towards the random point
+                new_node = extend_tree_towards(parent_node, random_point, max_step_size=STEP_SIZE)
 
-    ax.set_xlim(0, env.width)
-    ax.set_ylim(0, env.height)
-    ax.set_title(f"Step {step}")
-    
-    # Save the current figure as an image every SAVE_INTERVAL steps
-    #if step % SAVE_INTERVAL == 0:
-    #filename = os.path.join(folder_path, f"step_{step}.png")
-    filename = os.path.join(folder_path, f"step_{step:04d}.png")  # Zero-padded to 4 digits
-    plt.savefig(filename, dpi=100)
-    plt.close(fig)
+                # Check if the new node is collision-free; if so, break out of the loop
+                if env.is_collision_free(new_node):
+                    global_nodes.append(new_node)
+                    break  # Exit the resampling loop if the node is valid
 
 
 
-def draw_shortest_path(ax, nodes, goal_node, goal_reached ):
-    # Find the nearest node to the goal (if the goal isn't reached directly)
-    nearest_node = get_nearest_node(nodes, goal_node)
-    
-    # Trace the path back from the goal (or the nearest node) to the start
-    path = []
-    current_node = nearest_node
-    while current_node is not None:
-        path.append(current_node)
-        current_node = current_node.parent
-    
-    # Reverse the path to go from start to goal
-    path = path[::-1]
+            elif algorithm_choice == "RRT":
+                # Select the nearest node to the random point in RRT
+                #parent_node = min(global_nodes, key=lambda node: straight_line_distance_to_goal(node, random_point))
+                parent_node = get_nearest_node(global_nodes, random_point)
+                
+                # Try extending towards the random point
+                new_node = extend_tree_towards(parent_node, random_point, max_step_size=STEP_SIZE)
 
-    color = 'b-'
-    if goal_reached:
-        color = 'r-'
-
-    # Draw the path
-    for i in range(len(path) - 1):
-        ax.plot([path[i].x, path[i+1].x], [path[i].y, path[i+1].y], color, linewidth=2)
+                # Check if the new node is collision-free; if so, break out of the loop
+                if env.is_collision_free(new_node):
+                    global_nodes.append(new_node)
+                    break  # Exit the resampling loop if the node is valid
 
 
 
-# ex 1.1 rand tree with no actual plan
-def planning_algorithm(start_node, goal_node, env, max_steps=MAX_STEPS, folder_path=GIF_FOLDER):
-    nodes = [start_node]
-    step_count = 0
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    
-    while step_count < max_steps:
-        random_node = sample_random_node(nodes) #get a rand node 
-        random_point = sample_random_point(env) # get a rand point in the env
-        new_node = extend_towards_random(random_node, random_point, max_step_size=STEP_SIZE)
+            elif algorithm_choice == "RRT*":
+                # For RRT*, connect to nearest node but optimize the path with rewiring
+                #parent_node = min(nodes, key=lambda node: straight_line_distance_to_goal(node, random_point))
+                parent_node = get_nearest_node(global_nodes, random_point)
+
+                new_node = extend_tree_towards(parent_node, random_point, max_step_size=STEP_SIZE)
+
+                if env.is_collision_free(new_node):
+                    # Check nearby nodes within a radius to rewire
+                    neighbors = find_neighbors(global_nodes, new_node) #, radius=REWIRE_RADIUS)
+                    best_parent = min(neighbors, key=lambda n: n.cost + node_distance(n, new_node))
+
+                    # Connect the new node to the best parent
+                    new_node.parent = best_parent
+                    new_node.cost = best_parent.cost + node_distance(best_parent, new_node)
+
+                    # Rewire neighbors to the new node if it provides a shorter path
+                    for neighbor in neighbors:
+                        new_cost = new_node.cost + node_distance(new_node, neighbor)
+                        if new_cost < neighbor.cost:
+                            neighbor.parent = new_node
+                            neighbor.cost = new_cost
+
+                    global_nodes.append(new_node)
+                    break
+                
+
+
+        if global_goal_reached:
+            shortest_path_length = draw_environment(env, global_nodes, goal_node, global_step_count, folder_path=GIF_FOLDER, goal_radius=GOAL_RADIUS, goal_reached=global_goal_reached)
+        else: # if it wasn't reached before eval  the new node 
+            global_goal_reached = check_goal_reached(new_node, goal_node, goal_radius=GOAL_RADIUS)
+            shortest_path_length = draw_environment(env, global_nodes, goal_node, global_step_count, folder_path=GIF_FOLDER, goal_radius=GOAL_RADIUS, goal_reached=global_goal_reached)
+
         
+        global_step_count += 1
+        frame_counter = global_step_count - 1
+        final_frame_path = os.path.join(GIF_FOLDER, f"step_{frame_counter:04d}.png")
         
-        # Only add node if it is collision-free
-        if env.is_collision_free(new_node):  
-            nodes.append(new_node)
+        if os.path.exists(final_frame_path):
+            frame = Image.open(final_frame_path).convert("RGB")
+            frame.save(FINAL_FRAME_PATH)  # Save the final frame explicitly as RGB
+            yield frame, None  # Yield the frame and None as a placeholder for GIF
+            
+        if global_goal_reached:
+            if shortest_path_length < global_shortest_cost:
+                global_shortest_cost = shortest_path_length
+                break
+    
         
-
-        draw_environment(env, nodes, goal_node, step=step_count, folder_path=folder_path)
-
-        if check_goal_reached(new_node, goal_node, goal_radius=GOAL_RADIUS):        
-            draw_environment(env, nodes, goal_node, step=step_count, folder_path=folder_path, goal_reached = True)
-
-            print("Goal reached!")
-            break
+    end_time = time.time()  # End timer
+    elapsed_time = end_time - start_time
+    print(f"Simulation ran for {elapsed_time:.2f} seconds.")
     
-        step_count += 1
-        if step_count % SAVE_INTERVAL == 0:
-            gc.collect()  # Trigger garbage collection every SAVE_INTERVAL steps for efficiency
+
+    # Generate GIF only at the end of the steps
+    gif_path = os.path.join(GIF_FOLDER, "simulation.gif")
+    create_gif(GIF_FOLDER, gif_name=gif_path, duration=max(global_step_count,200) )
+    yield frame, gif_path  # Yield final frame and the GIF path after completion of steps
+
+# Reset function for simulation
+def reset_simulation():
+    global global_nodes, global_step_count, global_goal_reached, global_shortest_cost
+    global_nodes = None
+    global_step_count = 0
+    global_goal_reached = False
+    if os.path.exists(FINAL_FRAME_PATH):
+        os.remove(FINAL_FRAME_PATH)
+    return None, None, None
+
+
+
+# Gradio UI update with slider for heuristic probability
+with gr.Blocks() as demo:
+    gr.Markdown("### RT/RRT/RRT*")
+
+    algorithm_choice = gr.Radio(["RT", "RRT", "RRT*"], label="Algorithm Choice", value="RT")    
+
+    with gr.Row():
+        heuristic_slider = gr.Slider(minimum=0, maximum=1, value=1, step=0.01, label="Heuristic Probability")
     
-    if step_count == max_steps:
-        print(f"Reached max steps ({max_steps}) without finding the goal.")
-    return nodes
+    with gr.Row():
+        add_1_button = gr.Button("Add 1 Step")
+        add_10_button = gr.Button("Add 10 Steps")
+        add_50_button = gr.Button("Add 50 Steps")
+        add_100_button = gr.Button("Add 100 Steps")
+        add_500_button = gr.Button("Add 500 Steps")
+        reset_button = gr.Button("Reset Simulation")
+    
+    with gr.Row():
+        final_frame_output = gr.Image(type="pil", label="Current graph", streaming=True)
+        gif_output = gr.Image(type="filepath", label="GIF")
+
+    #button funcs: pass args
+    add_1_button.click(run_simulation_streaming, inputs=[gr.Number(value=1), algorithm_choice], outputs=[final_frame_output, gif_output])
+    add_10_button.click(run_simulation_streaming, inputs=[gr.Number(value=10), algorithm_choice], outputs=[final_frame_output, gif_output])
+    add_50_button.click(run_simulation_streaming, inputs=[gr.Number(value=50), algorithm_choice], outputs=[final_frame_output, gif_output])
+    add_100_button.click(run_simulation_streaming, inputs=[gr.Number(value=100), algorithm_choice], outputs=[final_frame_output, gif_output])
+    add_500_button.click(run_simulation_streaming, inputs=[gr.Number(value=500), algorithm_choice], outputs=[final_frame_output, gif_output])
+    reset_button.click(reset_simulation, outputs=[final_frame_output, gif_output])
 
 
-def frame_generator(folder_path):
-    for filename in sorted(os.listdir(folder_path)):
-        if filename.endswith('.png'):
-            yield Image.open(os.path.join(folder_path, filename))
-
-
-def create_gif(folder_path, gif_name='planning.gif', duration=200):
-    first_frame = next(frame_generator(folder_path))  # Get the first frame
-    first_frame.save(
-        gif_name,
-        save_all=True,
-        append_images=frame_generator(folder_path),
-        duration=duration,
-        loop=0
-    )
-# 
-# def create_gif(folder_path, gif_name='planning.gif', duration=200):
-    # frames = [Image.open(os.path.join(folder_path, f)) for f in sorted(os.listdir(folder_path)) if f.endswith('.png')]
-    # frames[0].save(gif_name, save_all=True, append_images=frames[1:], duration=duration, loop=0)
-
-
-nodes = planning_algorithm(start_node, goal_node, env, max_steps=MAX_STEPS, folder_path=GIF_FOLDER)
-create_gif(GIF_FOLDER, gif_name='planning.gif') #, duration=200)
+demo.launch()
